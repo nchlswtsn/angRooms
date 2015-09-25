2a23672d30ccc1b0e06f49fa2ffa5757ccecb79b
Darien Lombardi <darien.lombardi@gmail.com>
2015-09-24 21:02:54 -0700

    initial commit

---
+++ app/models/user.js
@@ -0,0 +1,33 @@
+// load the things we need
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+var mongoose = require('mongoose');
+
+// define the schema for our user model
+var userSchema = mongoose.Schema({
+  local: {
+    name: String,
+    email : String,
+    password: String,
+    transactions: [{
+      amount: Number,
+      createdAt: Date
+    }]
+  }
+});
+
+var User = mongoose.model('User', userSchema);
+
+
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
